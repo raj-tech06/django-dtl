6088eaf71a6623c4108f3bfbb35645ac6288e51b
Abhishek Sahu <rjdeveloper07@gmail.com>
2025-04-09 21:56:22 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def registerdata(request):
+    print(request.method)
+    print(request.POST)
+    print(request.GET)
+    print(request.FILES)
+    print(request.COOKIES)
+    print(request.META)
+    # return HttpResponse(request.POST)
+
+
+# def landing_page(request):
+#     return HttpResponse("hello")
