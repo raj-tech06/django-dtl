eae08741278e67c6973d112d2d35f968c987e6aa
Abhishek Sahu <rjdeveloper07@gmail.com>
2025-04-17 21:14:22 +0530

    login page create

--- myapp/models.py
+++ myapp/models.py
@@ -1,29 +1,5 @@
-# from django.db import models
-
-# # Create your models here.
-
-
-# class Student(models.Model):
-#     Stu_name = models.CharField(max_length=100)
-#     Stu_email = models.EmailField()
-#     Stu_address = models.CharField(max_length=200, null=True, blank=True)
-#     Stu_dis = models.TextField(null=True, blank=True)
-#     Stu_contact = models.CharField(max_length=15)
-#     Stu_dob = models.DateField()
-#     Stu_qualification = models.CharField(max_length=100, null=True, blank=True)
-#     Stu_gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female')])
-#     Stu_image = models.ImageField(upload_to='students/', null=True, blank=True)
-#     Stu_document = models.FileField(upload_to='documents/', null=True, blank=True)
-#     Stu_password = models.CharField(max_length=255)
-
-#     # def __str__(self):
-#     #     return self.Stu_name
-
-
-
 from django.db import models
 
-# Create your models here.
 
 class Student(models.Model):
     Stu_name = models.CharField(max_length=100)
